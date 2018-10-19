b33a95681b8173ade2b593cac0ef32a490a493a3
acyang <acyang0903@gmail.com>
2018-10-19 15:02:47 +0800

    Update and rename autologin.py to autologin/windows/autologin.py

--- autologin/windows/autologin.py
+++ autologin/windows/autologin.py
@@ -8,11 +8,11 @@ Created on Thu Oct 18 23:36:25 2018
 import pyotp
 import subprocess
 import getpass
-totp = pyotp.TOTP("CBUPKACCSXQS7GLI5AH72QCG4VJYJRGKUDMW5TL4YGW7X5XEE7BQ====")
+totp = pyotp.TOTP("your_OTP_KEY")
 print("Current OTP:", totp.now())
 
 address="140.110.148.11"
 username=input('USERNAME: ')
 password=getpass.getpass('PASSWORD: ')
 arg='sftp://'+username+':'+password+totp.now()+'@'+address+'/'
-subprocess.call(['D:\PortablePrograms\Winscp\WinSCP.exe', arg ], shell=True)
+subprocess.call(['PATH\TO\WinSCP.exe', arg ], shell=True)
