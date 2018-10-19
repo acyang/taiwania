05fc44feb479e13d1926d848b6af38a73b0566c7
acyang <acyang0903@gmail.com>
2018-10-19 21:09:20 +0800

    Update autologin.py

--- autologin/windows/autologin.py
+++ autologin/windows/autologin.py
@@ -8,11 +8,33 @@ Created on Thu Oct 18 23:36:25 2018
 import pyotp
 import subprocess
 import getpass
-totp = pyotp.TOTP("your_OTP_KEY")
+import time
+totp = pyotp.TOTP("YOUR_OTPPRIVATE_KEY")
+current_otp=totp.now()
 print("Current OTP:", totp.now())
 
 address="140.110.148.11"
 username=input('USERNAME: ')
 password=getpass.getpass('PASSWORD: ')
-arg='sftp://'+username+':'+password+totp.now()+'@'+address+'/'
-subprocess.call(['PATH\TO\WinSCP.exe', arg ], shell=True)
+
+commnd1='PATH\TO\WINSCP\WinSCP.exe'
+arg1='sftp://'+username+':'+password+totp.now()+'@'+address+'/'
+
+subprocess.call([commnd1, arg1 ], shell=True)
+
+if( totp.verify(current_otp)==True): # => True
+    print("Current OTP:", totp.now(), "is still work")
+else:
+    print("Current OTP:", totp.now(), "is not still work, wait for 30s")
+    time.sleep(30)
+    print("Renew OTP")
+    print("Current OTP:", totp.now())
+
+commnd2='PATH\TO\PUTTY\putty.exe'
+arg2="-ssh"
+arg3="-X"
+arg4=username+"@"+address
+arg5="-pw"
+arg6=password+totp.now()
+
+subprocess.call([commnd2, arg2, arg3, arg4, arg5, arg6 ], shell=True)
