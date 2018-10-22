7ad1b5f0fe029dfb1ddd666ac082cf1354d0e83b
acyang <acyang0903@gmail.com>
2018-10-22 10:43:36 +0800

    Update autologin.py
    1. change from subprocess.call to subprocess.Popen.
    2. wait for new OTP to be generated.

--- autologin/windows/autologin.py
+++ autologin/windows/autologin.py
@@ -9,7 +9,7 @@ import pyotp
 import subprocess
 import getpass
 import time
-totp = pyotp.TOTP("YOUR_OTPPRIVATE_KEY")
+totp = pyotp.TOTP("YOUR_OTP_PRIVATE_KEY")
 current_otp=totp.now()
 print("Current OTP:", totp.now())
 
@@ -17,24 +17,29 @@ address="140.110.148.11"
 username=input('USERNAME: ')
 password=getpass.getpass('PASSWORD: ')
 
-commnd1='PATH\TO\WINSCP\WinSCP.exe'
-arg1='sftp://'+username+':'+password+totp.now()+'@'+address+'/'
-
-subprocess.call([commnd1, arg1 ], shell=True)
-
-if( totp.verify(current_otp)==True): # => True
-    print("Current OTP:", totp.now(), "is still work")
-else:
-    print("Current OTP:", totp.now(), "is not still work, wait for 30s")
-    time.sleep(30)
-    print("Renew OTP")
-    print("Current OTP:", totp.now())
-
-commnd2='PATH\TO\PUTTY\putty.exe'
+commnd2='PATH\TO\putty.exe'
 arg2="-ssh"
 arg3="-X"
 arg4=username+"@"+address
 arg5="-pw"
 arg6=password+totp.now()
 
-subprocess.call([commnd2, arg2, arg3, arg4, arg5, arg6 ], shell=True)
+#subprocess.call([commnd2, arg2, arg3, arg4, arg5, arg6 ], shell=True)
+subprocess.Popen([commnd2, arg2, arg3, arg4, arg5, arg6 ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+#subprocess.Popen(['mspaint.exe'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+count = 0
+while count < 30:
+    time.sleep(1)
+    count += 1
+    if totp.verify(current_otp)!=True:
+        print("renew OTP done.")
+        break
+    print("Wait for new OTP "+str(count)+"/30")
+
+commnd1='PATH\TO\WinSCP.exe'
+arg1='sftp://'+username+':'+password+totp.now()+'@'+address+'/'
+
+#subprocess.call([commnd1, arg1 ], shell=True)
+subprocess.Popen([commnd1, arg1], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+#subprocess.Popen(['notepad.exe'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
